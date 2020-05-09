00fd391dcc53b085815a7f57bd33ea38d0719ef5
Michael Sours <msours@magicleap.com>
2020-05-09 13:51:53 -0400

    convert Cell Array input data types to be C++
    Using an older project of mine as a template, convert from C++/CLI input data types to C++

--- MathWorks.Cpp/include/CellArray.h
+++ MathWorks.Cpp/include/CellArray.h
@@ -1,19 +1,16 @@
 #define CellArray_H
 
-#pragma managed
+#pragma once
+
 #include "MathWorks/mat.h"
 #include "MathWorks/matrix.h"
 #include <Windows.h>
 
-#pragma once
-
-using namespace System;
-
-namespace mMatfile
+namespace MathWorks
 {
-	ref class MatlabStruct;
+	class MatlabStruct;
 
-	public ref class CellArray : IDisposable
+	class CellArray
 	{
 	public:
 		mxArray *Destination;
@@ -27,21 +24,21 @@ namespace mMatfile
 		CellArray(int Rows, int Cols);
 		~CellArray();
 		void Add(double Data, int InsertRow, int InsertCol);
-		void Add(array<double>^ Data, int Rows, int Cols, int InsertRow, int InsertCol);
-		void Add(array<double>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
+		void Add(const std::vector<double> &Data, int Rows, int Cols, int InsertRow, int InsertCol);
+		void Add(const std::vector<double> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
 		void Add(float Data, int InsertRow, int InsertCol);
-		void Add(array<float>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
-		void Add(array<INT8>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
-		void Add(array<INT16>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
-		void Add(array<INT32>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
-		void Add(array<INT64>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
-		void Add(array<UINT8>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
-		void Add(array<UINT16>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
-		void Add(array<UINT32>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
-		void Add(array<UINT64>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
-		void Add(String^ Data, int InsertRow, int InsertCol);
-		void Add(CellArray^ Data, int InsertRow, int InsertCol);
-		void Add(MatlabStruct^ Data, int InsertRow, int InsertCol);
+		void Add(const std::vector<float> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
+		void Add(const std::vector<INT8> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
+		void Add(const std::vector<int16_t> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
+		void Add(const std::vector<int32_t> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
+		void Add(const std::vector<int64_t> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
+		void Add(const std::vector<uint8_t> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
+		void Add(const std::vector<uint16_t> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
+		void Add(const std::vector<uint32_t> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
+		void Add(const std::vector<uint64_t> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol);
+		void Add(const std::string &Data, int InsertRow, int InsertCol);
+		void Add(CellArray &Data, int InsertRow, int InsertCol);
+		void Add(MatlabStruct &Data, int InsertRow, int InsertCol);
 		void Add(int Data, int InsertRow, int InsertCol);
 		void Add(bool Data, int InsertRow, int InsertCol);
 	};
--- MathWorks.Cpp/source/CellArray.cpp
+++ MathWorks.Cpp/source/CellArray.cpp
@@ -2,7 +2,7 @@
 #include "CellArray.h"
 #include <vector>
 
-namespace mMatfile 
+namespace MathWorks
 {
 	CellArray::CellArray(int Rows, int Cols)
 	{
@@ -22,33 +22,24 @@ namespace mMatfile
 	void CellArray::Add(double Data, int InsertRow, int InsertCol)
 	{
 		Source = mxCreateDoubleScalar(Data);
-
+		
 		memcpy((void *)(mxGetPr(Source)), (void *)&Data, sizeof(double));
 
-		int Ind = (InsertCol - 1)*this->Rows + InsertRow - 1;
+		int Ind = (InsertCol - 1) * this->Rows + InsertRow - 1;
 		mxSetCell(Destination, Ind, Source);
 	}
-	void CellArray::Add(array<double>^ Data, int Rows, int Cols, int InsertRow, int InsertCol)
+	void CellArray::Add(const std::vector<double> &Data, int Rows, int Cols, int InsertRow, int InsertCol)
 	{
 		int Length = Rows * Cols;
 
 		Source = mxCreateDoubleMatrix(Rows, Cols, mxREAL);
 
-		std::vector<double> data;
-
-		double V;
-		for (int k = 0; k < Length; k++)
-		{
-			V = Data[k];
-			data.push_back(V);
-		}
-
-		memcpy((void *)(mxGetPr(Source)), (void *)data.data(), sizeof(double)*Length);
+		memcpy((void *)(mxGetPr(Source)), (void *)Data.data(), sizeof(double)*Length);
 
 		int Ind = (InsertCol - 1)*this->Rows + InsertRow - 1;
 		mxSetCell(Destination, Ind, Source);
 	}
-	void CellArray::Add(array<double>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
+	void CellArray::Add(const std::vector<double> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
 	{
 		int Length = Rows * Cols*Dim3;
 
@@ -59,16 +50,7 @@ namespace mMatfile
 
 		Source = mxCreateNumericArray(3, Dim, mxDOUBLE_CLASS, mxREAL);
 
-		std::vector<double> data;
-
-		double V;
-		for (int k = 0; k < Length; k++)
-		{
-			V = Data[k];
-			data.push_back(V);
-		}
-
-		memcpy((void *)(mxGetPr(Source)), (void *)data.data(), sizeof(double)*Length);
+		memcpy((void *)(mxGetPr(Source)), (void *)Data.data(), sizeof(double)*Length);
 
 		int Ind = (InsertCol - 1)*this->Rows + InsertRow - 1;
 		mxSetCell(Destination, Ind, Source);
@@ -100,7 +82,7 @@ namespace mMatfile
 		int Ind = (InsertCol - 1)*this->Rows + InsertRow - 1;
 		mxSetCell(Destination, Ind, Source);
 	}
-	void CellArray::Add(array<float>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
+	void CellArray::Add(const std::vector<float> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
 	{
 		int Length = Rows * Cols*Dim3;
 
@@ -111,21 +93,12 @@ namespace mMatfile
 
 		Source = mxCreateNumericArray(3, Dim, mxSINGLE_CLASS, mxREAL);
 
-		std::vector<float> data;
+		memcpy((void *)(mxGetPr(Source)), (void *)Data.data(), sizeof(float)*Length);
 
-		float V;
-		for (int k = 0; k < Length; k++)
-		{
-			V = Data[k];
-			data.push_back(V);
-		}
-
-		memcpy((void *)(mxGetPr(Source)), (void *)data.data(), sizeof(float)*Length);
-
-		int Ind = (InsertCol - 1)*this->Rows + InsertRow - 1;
+		int Ind = (InsertCol - 1) * this->Rows + InsertRow - 1;
 		mxSetCell(Destination, Ind, Source);
 	}
-	void CellArray::Add(array<INT8>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
+	void CellArray::Add(const std::vector<INT8> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
 	{
 		int Length = Rows * Cols*Dim3;
 
@@ -136,21 +109,12 @@ namespace mMatfile
 
 		Source = mxCreateNumericArray(3, Dim, mxINT8_CLASS, mxREAL);
 
-		std::vector<INT8> data;
-
-		INT8 V;
-		for (int k = 0; k < Length; k++)
-		{
-			V = Data[k];
-			data.push_back(V);
-		}
-
-		memcpy((void *)(mxGetPr(Source)), (void *)data.data(), sizeof(INT8)*Length);
+		memcpy((void *)(mxGetPr(Source)), (void *)Data.data(), sizeof(INT8)*Length);
 
 		int Ind = (InsertCol - 1)*this->Rows + InsertRow - 1;
 		mxSetCell(Destination, Ind, Source);
 	}
-	void CellArray::Add(array<INT16>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
+	void CellArray::Add(const std::vector<int16_t> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
 	{
 		int Length = Rows * Cols*Dim3;
 
@@ -161,21 +125,12 @@ namespace mMatfile
 
 		Source = mxCreateNumericArray(3, Dim, mxINT16_CLASS, mxREAL);
 
-		std::vector<INT16> data;
-
-		INT16 V;
-		for (int k = 0; k < Length; k++)
-		{
-			V = Data[k];
-			data.push_back(V);
-		}
-
-		memcpy((void *)(mxGetPr(Source)), (void *)data.data(), sizeof(INT16)*Length);
+		memcpy((void *)(mxGetPr(Source)), (void *)Data.data(), sizeof(INT16)*Length);
 
 		int Ind = (InsertCol - 1)*this->Rows + InsertRow - 1;
 		mxSetCell(Destination, Ind, Source);
 	}
-	void CellArray::Add(array<INT32>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
+	void CellArray::Add(const std::vector<int32_t> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
 	{
 		int Length = Rows * Cols*Dim3;
 
@@ -186,21 +141,12 @@ namespace mMatfile
 
 		Source = mxCreateNumericArray(3, Dim, mxINT32_CLASS, mxREAL);
 
-		std::vector<INT32> data;
+		memcpy((void *)(mxGetPr(Source)), (void *)Data.data(), sizeof(INT32)*Length);
 
-		INT32 V;
-		for (int k = 0; k < Length; k++)
-		{
-			V = Data[k];
-			data.push_back(V);
-		}
-
-		memcpy((void *)(mxGetPr(Source)), (void *)data.data(), sizeof(INT32)*Length);
-
-		int Ind = (InsertCol - 1)*this->Rows + InsertRow - 1;
+		int Ind = (InsertCol - 1) * this->Rows + InsertRow - 1;
 		mxSetCell(Destination, Ind, Source);
 	}
-	void CellArray::Add(array<INT64>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
+	void CellArray::Add(const std::vector<int64_t> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
 	{
 		int Length = Rows * Cols*Dim3;
 
@@ -211,21 +157,12 @@ namespace mMatfile
 
 		Source = mxCreateNumericArray(3, Dim, mxINT64_CLASS, mxREAL);
 
-		std::vector<INT64> data;
+		memcpy((void *)(mxGetPr(Source)), (void *)Data.data(), sizeof(INT64)*Length);
 
-		INT64 V;
-		for (int k = 0; k < Length; k++)
-		{
-			V = Data[k];
-			data.push_back(V);
-		}
-
-		memcpy((void *)(mxGetPr(Source)), (void *)data.data(), sizeof(INT64)*Length);
-
-		int Ind = (InsertCol - 1)*this->Rows + InsertRow - 1;
+		int Ind = (InsertCol - 1) * this->Rows + InsertRow - 1;
 		mxSetCell(Destination, Ind, Source);
 	}
-	void CellArray::Add(array<UINT8>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
+	void CellArray::Add(const std::vector<uint8_t> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
 	{
 		int Length = Rows * Cols*Dim3;
 
@@ -236,21 +173,12 @@ namespace mMatfile
 
 		Source = mxCreateNumericArray(3, Dim, mxUINT8_CLASS, mxREAL);
 
-		std::vector<UINT8> data;
-
-		UINT8 V;
-		for (int k = 0; k < Length; k++)
-		{
-			V = Data[k];
-			data.push_back(V);
-		}
-
-		memcpy((void *)(mxGetPr(Source)), (void *)data.data(), sizeof(UINT8)*Length);
+		memcpy((void *)(mxGetPr(Source)), (void *)Data.data(), sizeof(UINT8)*Length);
 
 		int Ind = (InsertCol - 1)*this->Rows + InsertRow - 1;
 		mxSetCell(Destination, Ind, Source);
 	}
-	void CellArray::Add(array<UINT16>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
+	void CellArray::Add(const std::vector<uint16_t> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
 	{
 		int Length = Rows * Cols*Dim3;
 
@@ -261,21 +189,12 @@ namespace mMatfile
 
 		Source = mxCreateNumericArray(3, Dim, mxUINT16_CLASS, mxREAL);
 
-		std::vector<UINT16> data;
+		memcpy((void *)(mxGetPr(Source)), (void *)Data.data(), sizeof(UINT16)*Length);
 
-		UINT16 V;
-		for (int k = 0; k < Length; k++)
-		{
-			V = Data[k];
-			data.push_back(V);
-		}
-
-		memcpy((void *)(mxGetPr(Source)), (void *)data.data(), sizeof(UINT16)*Length);
-
-		int Ind = (InsertCol - 1)*this->Rows + InsertRow - 1;
+		int Ind = (InsertCol - 1) * this->Rows + InsertRow - 1;
 		mxSetCell(Destination, Ind, Source);
 	}
-	void CellArray::Add(array<UINT32>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
+	void CellArray::Add(const std::vector<uint32_t> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
 	{
 		int Length = Rows * Cols*Dim3;
 
@@ -286,21 +205,12 @@ namespace mMatfile
 
 		Source = mxCreateNumericArray(3, Dim, mxUINT32_CLASS, mxREAL);
 
-		std::vector<UINT32> data;
-
-		UINT32 V;
-		for (int k = 0; k < Length; k++)
-		{
-			V = Data[k];
-			data.push_back(V);
-		}
-
-		memcpy((void *)(mxGetPr(Source)), (void *)data.data(), sizeof(UINT32)*Length);
+		memcpy((void *)(mxGetPr(Source)), (void *)Data.data(), sizeof(UINT32)*Length);
 
 		int Ind = (InsertCol - 1)*this->Rows + InsertRow - 1;
 		mxSetCell(Destination, Ind, Source);
 	}
-	void CellArray::Add(array<UINT64>^ Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
+	void CellArray::Add(const std::vector<uint64_t> &Data, int Rows, int Cols, int Dim3, int InsertRow, int InsertCol)
 	{
 		int Length = Rows * Cols*Dim3;
 
@@ -311,44 +221,33 @@ namespace mMatfile
 
 		Source = mxCreateNumericArray(3, Dim, mxUINT64_CLASS, mxREAL);
 
-		std::vector<UINT64> data;
+		memcpy((void *)(mxGetPr(Source)), (void *)Data.data(), sizeof(UINT64)*Length);
 
-		UINT64 V;
-		for (int k = 0; k < Length; k++)
-		{
-			V = Data[k];
-			data.push_back(V);
-		}
-
-		memcpy((void *)(mxGetPr(Source)), (void *)data.data(), sizeof(UINT64)*Length);
-
-		int Ind = (InsertCol - 1)*this->Rows + InsertRow - 1;
+		int Ind = (InsertCol - 1) * this->Rows + InsertRow - 1;
 		mxSetCell(Destination, Ind, Source);
 	}
-	void CellArray::Add(String^ Data, int InsertRow, int InsertCol)
+	void CellArray::Add(const std::string &Data, int InsertRow, int InsertCol)
 	{
-		const char *V = (char*)(void*)System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(Data);
+		const char *V = Data.c_str();
 		Source = mxCreateString(V);
 
-		int Ind = (InsertCol - 1)*this->Rows + InsertRow - 1;
+		int Ind = (InsertCol - 1) * this->Rows + InsertRow - 1;
 		mxSetCell(Destination, Ind, Source);
-
-		System::Runtime::InteropServices::Marshal::FreeHGlobal(System::IntPtr((void*)V));
 	}
-	void CellArray::Add(CellArray^ Data, int InsertRow, int InsertCol)
+	void CellArray::Add(CellArray &Data, int InsertRow, int InsertCol)
 	{
 		int Ind = (InsertCol - 1)*this->Rows + InsertRow - 1;
-		mxSetCell(Destination, Ind, Data->Destination);
+		mxSetCell(Destination, Ind, Data.Destination);
 
 		this->HighestStructure = true;
-		Data->HighestStructure = false;
+		Data.HighestStructure = false;
 	}
-	void CellArray::Add(MatlabStruct^ Data, int InsertRow, int InsertCol)
+	void CellArray::Add(MatlabStruct &Data, int InsertRow, int InsertCol)
 	{
 		int Ind = (InsertCol - 1)*(this->Rows) + InsertRow - 1;
-		mxSetCell(Destination, Ind, Data->Destination);
+		mxSetCell(Destination, Ind, Data.Destination);
 
 		this->HighestStructure = true;
-		Data->HighestStructure = false;
+		Data.HighestStructure = false;
 	}
 }
